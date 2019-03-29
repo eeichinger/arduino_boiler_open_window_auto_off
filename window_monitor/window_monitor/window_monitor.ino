81fee35f8aa27dc2f23987d4d998b3ebd4b51650
Eichinger, Erich <erich.eichinger@opencredo.com>
2019-03-29 19:12:14 +0000

    basic fm sender/receiver setup - working

---
+++ window_monitor/window_monitor/window_monitor.ino
@@ -0,0 +1,32 @@
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 1 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+// MONITOR/RIGHT
+
+RF24 radio(6, 7); // CE, CSN
+
+const byte address[6] = "00001";
+
+void setup() {
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+void loop() {
+  const char text[] = "OPEN";
+  radio.write(&text, sizeof(text));
+  delay(1000);
+  const char text[] = "CLOSED";
+  radio.write(&text, sizeof(text));
+  delay(1000);
+}
