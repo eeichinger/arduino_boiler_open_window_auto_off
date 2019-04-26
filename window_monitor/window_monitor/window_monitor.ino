0312d44aafc12b193a4eb894fe90dac82aa22961
Eichinger, Erich <erich.eichinger@opencredo.com>
2019-04-26 21:57:56 +0100

    add power-saving from sample add https://tmrh20.github.io/RF24/pingpair_sleepy_8ino-example.html

--- window_monitor/window_monitor/window_monitor.ino
+++ window_monitor/window_monitor/window_monitor.ino
@@ -9,6 +9,12 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+//#include <JeeLib.h>
+#include <avr/sleep.h>
+#include <avr/power.h>
+typedef enum { wdt_16ms = 0, wdt_32ms, wdt_64ms, wdt_128ms, wdt_250ms, wdt_500ms, wdt_1s, wdt_2s, wdt_4s, wdt_8s } wdt_prescalar_e;
+void setup_watchdog(uint8_t prescalar);
+void do_sleep(void);
 
 
 // Constants & Variables
@@ -23,13 +29,18 @@ void setup() {
   Serial.begin(9600);
   pinMode(REED_PIN, INPUT_PULLUP);
 
+  setup_watchdog(wdt_4s);
+
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_LOW);
-  radio.stopListening();
+  //  radio.stopListening();
 }
 
 void loop() {
+  radio.powerUp();                                // Power up the radio after sleeping
+  radio.stopListening();
+
   int proximity = digitalRead(REED_PIN); // Read the state of the switch
   Serial.print("reading switch: ");
   if (proximity == HIGH) // If the pin reads low, the switch is closed.
@@ -43,4 +54,48 @@ void loop() {
     radio.write(&CMD_OPEN, sizeof(CMD_OPEN));
     //    digitalWrite(LED_PIN, LOW); // Turn the LED off
   }
+  Serial.println("going to sleep...");
+  Serial.flush();
+
+  radio.powerDown();              // NOTE: The radio MUST be powered back up again manually
+  // Sleep the MCU.
+  do_sleep();
+  //  Sleepy::loseSomeTime(2000);
+}
+
+void wakeUp() {
+  sleep_disable();
+}
+// Sleep helpers
+//Prescalar values
+// 0=16ms, 1=32ms,2=64ms,3=125ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+void setup_watchdog(uint8_t prescalar) {
+  uint8_t wdtcsr = prescalar & 7;
+  if ( prescalar & 8 )
+    wdtcsr |= _BV(WDP3);
+  MCUSR &= ~_BV(WDRF);                      // Clear the WD System Reset Flag
+  WDTCSR = _BV(WDCE) | _BV(WDE);            // Write the WD Change enable bit to enable changing the prescaler and enable system reset
+  WDTCSR = _BV(WDCE) | wdtcsr | _BV(WDIE);  // Write the prescalar bits (how long to sleep, enable the interrupt to wake the MCU
+}
+
+//ISR(WDT_vect) {
+//  Sleepy::watchdogEvent();  // Setup the watchdog
+//}
+
+ISR(WDT_vect) {
+  //--sleep_cycles_remaining;
+//  Serial.println(F("WDT"));
+}
+
+void do_sleep(void) {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+  sleep_enable();
+  attachInterrupt(0, wakeUp, LOW);
+  WDTCSR |= _BV(WDIE);
+  sleep_mode();                        // System sleeps here
+  // The WDT_vect interrupt wakes the MCU from here
+  sleep_disable();                     // System continues execution here when watchdog timed out
+  detachInterrupt(0);
+  WDTCSR &= ~_BV(WDIE);
 }
