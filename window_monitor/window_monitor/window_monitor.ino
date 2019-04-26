9dbac27aa85d6ff45eb37110b8a9a2b726a3dfe1
Eichinger, Erich <erich.eichinger@opencredo.com>
2019-04-26 21:57:17 +0100

    code cleanup

--- window_monitor/window_monitor/window_monitor.ino
+++ window_monitor/window_monitor/window_monitor.ino
@@ -1,21 +1,21 @@
 /*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+  Arduino Wireless Communication Tutorial
+      Example 1 - Transmitter Code
+
+  by Dejan Nedelkovski, www.HowToMechatronics.com
+
+  Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
 */
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-// MONITOR/RIGHT
 
+// Constants & Variables
 RF24 radio(6, 7); // CE, CSN
 
 const byte address[6] = "00001";
-const int REED_PIN = 2; 
+const int REED_PIN = 2;
 const char CMD_OPEN[] = "OPEN";
 const char CMD_CLOSED[] = "CLOSED";
 
@@ -41,11 +41,6 @@ void loop() {
   {
     Serial.println("open");
     radio.write(&CMD_OPEN, sizeof(CMD_OPEN));
-//    digitalWrite(LED_PIN, LOW); // Turn the LED off
-  }  
-//  radio.write(&text, sizeof(text));
-//  delay(1000);
-//  const char text[] = "CLOSED";
-//  radio.write(&text, sizeof(text));
-  delay(500);
+    //    digitalWrite(LED_PIN, LOW); // Turn the LED off
+  }
 }
