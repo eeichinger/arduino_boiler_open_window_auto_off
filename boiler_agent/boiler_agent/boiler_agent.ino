3327db325075abd7711d012031f460d1e53983fe
Eichinger, Erich <erich.eichinger@opencredo.com>
2019-04-26 21:53:31 +0100

    add some LED indicator to boiler controller

--- boiler_agent/boiler_agent/boiler_agent.ino
+++ boiler_agent/boiler_agent/boiler_agent.ino
@@ -16,6 +16,7 @@ const int BUZZER_PIN = 8;
 RF24 radio(6, 7); // CE, CSN
 const byte address[6] = "00001";
 const int RELAY_PIN = 2; 
+const int LED_PIN = 13;
 const char CMD_OPEN[] = "OPEN";
 const char CMD_CLOSED[] = "CLOSED";
 const unsigned long last_message_timeout = 5000;
@@ -24,6 +25,8 @@ unsigned long last_message_millis = 0;
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
   digitalWrite(BUZZER_PIN, LOW); 
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
   
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH); 
@@ -44,14 +47,17 @@ void loop() {
     if(strcmp(text, CMD_OPEN) == 0) {
       Serial.println("switch off relay");
       digitalWrite(RELAY_PIN, LOW);
+      digitalWrite(LED_PIN, LOW);
       tone(BUZZER_PIN, NOTE_C5, 700);
       last_message_millis = millis();
     }
     if(strcmp(text, CMD_CLOSED) == 0) {
       Serial.println("switch on relay");
       digitalWrite(RELAY_PIN, HIGH);
+      digitalWrite(LED_PIN, HIGH);
       tone(BUZZER_PIN, NOTE_C5, 300);
       last_message_millis = millis();
+      delay(300);
     }
   } else {
     Serial.println(F("waiting"));
@@ -59,7 +65,11 @@ void loop() {
   if (millis() - last_message_millis > last_message_timeout) {
     Serial.println(F("more than timeout_interval passed since last OPEN message - turn on relay"));
     digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(LED_PIN, HIGH);
     tone(BUZZER_PIN, NOTE_C5, 50);
+    delay(100);
   }
+  digitalWrite(LED_PIN, LOW);
+  Serial.flush();
   delay(1000);
 }
