e4a087e6f4039123c23f315347e18af71e76c7aa
Eichinger, Erich <erich.eichinger@opencredo.com>
2019-08-30 09:56:36 +0100

    add buzzer for acoustic feedback

--- window_monitor/window_monitor/window_monitor.ino
+++ window_monitor/window_monitor/window_monitor.ino
@@ -23,13 +23,16 @@ RF24 radio(6, 7); // CE, CSN
 const byte address[6] = "00001";
 const int check_interval_ms = 4000; // interval in ms to check reed switch
 const int REED_PIN = 2;
+const int BUZZER_PIN = 9;
 const char CMD_OPEN[] = "OPEN";
 const char CMD_CLOSED[] = "CLOSED";
 
 void setup() {
   Serial.begin(9600);
   pinMode(REED_PIN, INPUT_PULLUP); // set NO reed pin to HIGH by default, only if the switch triggers it will go LOW.
-
+  pinMode(BUZZER_PIN, OUTPUT); // acoustic signal
+  digitalWrite(RELAY_PIN, LOW);
+  
   radio.begin();
   radio.openWritingPipe(address);
   radio.setPALevel(RF24_PA_LOW);
@@ -44,14 +47,19 @@ void loop() {
   if (proximity == HIGH) // If the pin reads low, the switch is closed.
   {
     Serial.println("closed");
+    digitalWrite(BUZZER_PIN, HIGH);
     radio.write(&CMD_CLOSED, sizeof(CMD_CLOSED));
+    delay(50);
   }
   else
   {
     Serial.println("open");
+    digitalWrite(BUZZER_PIN, HIGH);
     radio.write(&CMD_OPEN, sizeof(CMD_OPEN));
+    delay(100);
     //    digitalWrite(LED_PIN, LOW); // Turn the LED off
   }
+  digitalWrite(BUZZER_PIN, LOW);
   Serial.println("going to sleep...");
   Serial.flush();
 
